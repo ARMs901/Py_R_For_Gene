df0acc300440fcd75aefa7a46f1ea921a46a9551
Armand Rambottom <129251357+ARMs901@users.noreply.github.com>
2023-09-30 15:48:27 +0800

    Update 可视化交集基因.py

--- 可视化交集基因.py
+++ 可视化交集基因.py
@@ -14,11 +14,19 @@ def read_genes_from_file(file_path):
 genes_svm_rfe = read_genes_from_file("D:/BaiduNetdiskDownload/Diagnostic_video/136Diagnostic/14.venn/U_D/SVM-RFE.gene.txt")
 genes_lasso = read_genes_from_file("D:/BaiduNetdiskDownload/Diagnostic_video/136Diagnostic/14.venn/U_D/LASSO.gene.txt")
 genes_nn = read_genes_from_file("D:/BaiduNetdiskDownload/Diagnostic_video/136Diagnostic/14.venn/U_D/NN_selected_genes.txt")
+#genes_svm_rfe = read_genes_from_file("D:/BaiduNetdiskDownload/Diagnostic_video/136Diagnostic/14.venn/U_D/SVM-RFE_selected_features.txt")
 
 # 创建Venn图
 plt.figure(figsize=(10, 7))  # 设置图的大小
 # 使用venn3函数创建三个集合的Venn图，输入是三个基因集合，set_labels参数设置每个集合的标签
 v = venn3([genes_svm_rfe, genes_lasso, genes_nn], set_labels=['SVM-RFE', 'LASSO', 'NN'])
+
+# 计算三个基因列表的交集
+intersected_genes = genes_svm_rfe.intersection(genes_lasso).intersection(genes_nn)
+# 将交集基因写入文件
+with open("D:/BaiduNetdiskDownload/Diagnostic_video/136Diagnostic/14.venn/U_D/intersected_genes.txt", 'w') as f:
+    for gene in intersected_genes:
+        f.write(f"{gene}\n")
 
 # 保存Venn图为PNG格式到指定的位置
 plt.savefig("D:/BaiduNetdiskDownload/Diagnostic_video/136Diagnostic/14.venn/U_D/venn_diagram.png")
